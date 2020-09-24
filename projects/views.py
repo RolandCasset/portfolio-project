45baa502aef63f2d759e80d01740125780e1d015
RolandCasset <joshua5750@gmail.com>
2020-09-24 17:05:23 +1000

    Everything is working

--- projects/views.py
+++ projects/views.py
@@ -4,9 +4,9 @@ from .models import Project
 
 def allprojects(request):
     projects = Project.objects
-    return render(request, 'project/allprojects.html', {'project':projects})
+    return render(request, 'project/allprojects.html', {'projects':projects})
 
 
 def detail(request, project_id):
     detailprojects = get_object_or_404(Project, pk=project_id)
-    return render(request, 'project/detail.html', {'project':detailprojects})
+    return render(request, 'project/detail.html', {'projects':detailprojects})
